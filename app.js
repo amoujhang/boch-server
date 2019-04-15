f4fc84c52fa4df9091f7d8d94e4849b049b8903c
amou <amoujhang@13techart.com>
2019-04-16 01:58:32 +0800

    commet dp request

--- app.js
+++ app.js
@@ -121,6 +121,7 @@ app.post('/ss', function (req, res, next) {
 })
 
 //DP======================================
+/*
 request('http://192.168.2.100:3000/api/v1/material_guides_dropdownlist/1\?AuthToken\=tech13999', function (error, response, body) {
 	if (!error && response.statusCode == 200) {
 		console.log('get dplists0 status code 200')
@@ -145,6 +146,7 @@ request('http://192.168.2.100:3000/api/v1/material_guides_dropdownlist/3\?AuthTo
 		console.log('dplists2 status error')
 	}
 })
+*/
 app.get('/dplists', function (req, res, next) {
 	console.log("Dp query : " + req.query.id + ";");
 	//  res.json(success);
