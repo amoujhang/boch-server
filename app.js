20f06dbb87b240efa9da3e9ac5f5d97c9b941628
amou <amoujhang@13techart.com>
2019-04-16 01:48:38 +0800

    test droplist 0

--- app.js
+++ app.js
@@ -121,21 +121,19 @@ app.post('/ss', function (req, res, next) {
 })
 
 //DP======================================
-
+request('http://192.168.2.100:3000/api/v1/material_guides_dropdownlist/1\?AuthToken\=tech13999', function (error, response, body) {
+	if (!error && response.statusCode == 200) {
+		console.log('get dplists0 status code 200')
+		droplists = body
+	} else {
+		console.log('ss status error')
+	}
+})
 app.get('/dplists', function (req, res, next) {
 	console.log("Dp query : " + req.query.id + ";");
 	//  res.json(success);
 	if (req.query.id == '0') {
-		request('http://192.168.2.100:3000/api/v1/material_guides_dropdownlist/1\?AuthToken\=tech13999', function (error, response, body) {
-			if (!error && response.statusCode == 200) {
-				console.log('ss status code 200')
-				console.log(body) // 打印google首页
-				res.json(body);
-			} else {
-				console.log('ss status error')
-				res.json(droplists);
-			}
-		})		
+		res.json(droplists);
 	} else if (req.query.id == '1') {
 		res.json(droplistsmenu2);
 	} else {
